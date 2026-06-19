77a221e14b6f43542fb287570338b54a469ffd89
robocop72-encrypted <tusharupadhiya2@gmail.com>
2026-06-19 23:41:25 +0530

    added width and height pointer logic with dynamic memory allcoation to the width and height variable . used calloc

--- image_viewer/iv.c
+++ image_viewer/iv.c
@@ -4,8 +4,27 @@
 int main(){
     SDL_setenv("SDL_VIDEODRIVER", "x11", 1);
     //initialized the window pointer
-    const int width = 900;
-    const int height = 600;
+    FILE* in = stdin;
+    char* throwaway = calloc(1000,sizeof(char));
+    //Read first line (specifier P3 or P6 - ignore here)
+    fgets(throwaway,1000,in);
+    //Read second line (comment)
+      fgets(throwaway,1000,in);
+      
+    
+    //Read third line(dimensions: width/height)
+        
+    char* dimensions = calloc(1000,sizeof(char));
+    fgets(dimensions,1000,in);
+
+    //Read fourth line(max color value - ignore it)
+       fgets(throwaway,1000,in);
+     free(throwaway);
+    int width=-1 ;
+    int height=-1 ;
+    sscanf(dimensions,"%d %d\n",&width,&height);
+    printf("width: %d  height: %d",width,height );
+    free(dimensions);
     SDL_Window* window = SDL_CreateWindow("Window",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,0);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
     Uint8 r,g,b;

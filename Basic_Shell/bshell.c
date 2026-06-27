b5547615c3f9b500abcd0bd9e9ebfdeaeea054c0
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-06-27 21:39:38 +0530

    Made the loop and read part of the repl for the shell

--- Basic_Shell/bshell.c
+++ Basic_Shell/bshell.c
@@ -1,27 +1,29 @@
 #include "cell.h"
-char * cell_read_line(void){
-	char* buf;
-	size_t buf_size;
-	buf = NULL;
+
+// READ
+char* read_line(void){
+	char* buf = NULL;
+	size_t buf_size=0;
 	if(getline(&buf,&buf_size,stdin)==-1){
-		buf = NULL;
-		if(feof(stdin))
-			p(RED"[EOF]"RST);
-		else
-			p(RED"Getline failed!"RST);
-		p("%s\n",buf);
+		p(RED"[EOF]\n"RST);
+		free(buf);
+		return NULL;
 	}
+	buf[strcspn(buf,"\n")]='\0';
 	return buf;
 }
-int main(int argc, char **argv){
-	char * line;
+
+//LOOP
+int main(void){
+	char *line;
 	while(1){
+	p(BGREEN"bshell"RST CYAN" > "RST); 
+	fflush(stdout);
+		line = read_line();
+		if(line==NULL) break;
 
-	line = cell_read_line();
-	p("%s\n",line);
-	pause();
-
-	}
 	
+	free(line);
+	}
 	return EXIT_SUCCESS;
 }
--- Basic_Shell/cell.h
+++ Basic_Shell/cell.h
@@ -1,11 +1,18 @@
 #pragma once
 #include<stdlib.h>
+#include<string.h>
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
 #define p(...) printf(__VA_ARGS__)
 #define RST   "\033[0m"
-#define C    "\033[36m"
+#define CYAN    "\033[36m"
 #define G   "\033[32m"
 #define RED     "\033[31m"
 #define Y  "\033[1;33m"
+#define BRED     "\033[1;31m"
+#define BGREEN   "\033[1;32m"
+#define BYELLOW  "\033[1;33m"
+#define BBLUE    "\033[1;34m"
+#define BMAGENTA "\033[1;35m"
+#define BCYAN    "\033[1;36m"

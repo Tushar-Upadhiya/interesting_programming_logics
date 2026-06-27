bff000dd047850a0e385a851bc4e2cc95122ec25
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-06-27 11:33:47 +0530

    Made a basic REPL structure

--- Basic_Shell/bshell.c
+++ Basic_Shell/bshell.c
@@ -1,10 +1,27 @@
 #include "cell.h"
+char * cell_read_line(void){
+	char* buf;
+	size_t buf_size;
+	buf = NULL;
+	if(getline(&buf,&buf_size,stdin)==-1){
+		buf = NULL;
+		if(feof(stdin))
+			p(RED"[EOF]"RST);
+		else
+			p(RED"Getline failed!"RST);
+		p("%s\n",buf);
+	}
+	return buf;
+}
 int main(int argc, char **argv){
-	(void) argc;
-	int status;
-	if(fork()==0)
-		execvp(argv[1],argv+1);
-	wait(&status);
+	char * line;
+	while(1){
+
+	line = cell_read_line();
+	p("%s\n",line);
+	pause();
+
+	}
 	
 	return EXIT_SUCCESS;
 }
--- Basic_Shell/cell.h
+++ Basic_Shell/cell.h
@@ -3,3 +3,9 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#define p(...) printf(__VA_ARGS__)
+#define RST   "\033[0m"
+#define C    "\033[36m"
+#define G   "\033[32m"
+#define RED     "\033[31m"
+#define Y  "\033[1;33m"

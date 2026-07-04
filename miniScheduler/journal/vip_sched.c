43f9c7bc88f9fa2d5a91381ac9b351e202b7a20e
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-07-04 12:20:15 +0530

    creating pthread_create() with user defined attributes

--- miniScheduler/journal/vip_sched.c
+++ miniScheduler/journal/vip_sched.c
@@ -14,14 +14,18 @@ int main(){
 	struct sched_param param;
 
 	pthread_attr_init (&attr);
-
+	
+	//ignoring default settings
 	pthread_attr_setinitsched(&attr,PTHREAD_EXPLICIT_SCHED);
-
+	
+	//set the policy to round robin(SCHED_RR)
 	pthread_attr_setschedpolicy(&attr,SCHED_RR);
 
+	//set the priority(1-99 for SCHED_RR)
 	param.sched_priority=10;
 	pthread_attr_setschedparameter(&attr,&param);
-
+	
+	//passing the custom attribute object
 	pthread_create(&p,&attr,mythread,NULL);
 	pthread_join(p,NULL);
 

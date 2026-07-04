483687b2992ebf4fa6e8f5714505fb6308f11388
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-07-04 12:35:17 +0530

    Necessary bug check and commenting done

--- miniScheduler/journal/vip_sched.c
+++ miniScheduler/journal/vip_sched.c
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 
 void * mythread(void * arg){
-	printf("This thread is running with special scheduling.");
+	printf("This thread is running with special scheduling.\n");
 return NULL;
 }
 
@@ -16,14 +16,14 @@ int main(){
 	pthread_attr_init (&attr);
 	
 	//ignoring default settings
-	pthread_attr_setinitsched(&attr,PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setinheritsched(&attr,PTHREAD_EXPLICIT_SCHED);
 	
 	//set the policy to round robin(SCHED_RR)
 	pthread_attr_setschedpolicy(&attr,SCHED_RR);
 
 	//set the priority(1-99 for SCHED_RR)
 	param.sched_priority=10;
-	pthread_attr_setschedparameter(&attr,&param);
+pthread_attr_setschedparam(&attr,&param);
 	
 	//passing the custom attribute object
 	pthread_create(&p,&attr,mythread,NULL);
@@ -33,3 +33,7 @@ int main(){
 
 return 0;
 }
+
+
+//while execution it is necessary to use "sudo ./vip_sched" because running command without sudo will not work as in the code we have set the policy (ScHED-RR)  and that is a system level policy change from default policy for execution of threads
+

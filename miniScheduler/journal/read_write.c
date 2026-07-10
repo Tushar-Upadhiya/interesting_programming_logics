de8b8992502719a3e4136d2d169480ac184cbccd
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-07-10 21:05:28 +0530

    init: reader-writer problem.Wrote the writer logic in reader-writer problem

---
+++ miniScheduler/journal/read_write.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int reader_count = 0;
+pthread_mutex_t mutex;
+pthread_rwlock_t rwlock= PTHREAD_RWLOCK_INITIALIZER;
+
+void read_lock(){
+	pthread_rwlock_rdlock(&rwlock);
+}
+
+
+void read_unlock(){
+	pthread_rwlock_unlock(&rwlock);
+}
+
+void write_lock(){
+	pthread_rwlock_wrlock(&rwlock);
+}
+
+void write_unlock(){
+	pthread_rwlock_unlock(&rwlock);
+}
+
+void* reader(void* arg){
+	pthread_mutex_lock(&mutex);
+	reader_count++;
+	if(reader_count==1){
+		write_lock();
+	}
+	pthread_mutex_unlock(&mutex);
+
+	//read
+	
+	printf("Reader %d is reading!",*(int*)arg);
+	sleep(1);
+
+	pthread_mutex_lock(&mutex);
+	reader_count--;
+	if(reader_count==0){
+		write_unlock();
+	}
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
+}
+
+int main(){
+	return EXIT_SUCCESS;
+}
+
+
